70c1d5085629882c054151efe46de9d77b62b8c3
sudaneee <99607513+sudaneee@users.noreply.github.com>
2022-09-12 07:00:45 -0700

    Update settings.py

--- DjangoApp/settings.py
+++ DjangoApp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*&=8))+08y71!-%!qyk%lp!wwk-gt@wthl)%u%dibl&3j-c6uj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['178.79.178.160']
+ALLOWED_HOSTS = ['178.79.178.160', 'localhost', '127.0.0.1']
 
 
 # Application definition
