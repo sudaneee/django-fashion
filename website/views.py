14697cb3e8b7c246864dde3a102643153ef5ed66
sudaneee <99607513+sudaneee@users.noreply.github.com>
2022-10-04 08:21:34 -0700

    still commiting

--- website/views.py
+++ website/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Gallery, Service, Slider
+from .models import Gallery, Service, Slider, Staff
 
 # Create your views here.
 
@@ -8,10 +8,12 @@ def home(request):
     slider = Slider.objects.all()
     service = Service.objects.all()
     gallery = Gallery.objects.all()
+    staff = Staff.objects.all()
 
     context = {
         'sliders': slider,
         'services': service,
         'galleries': gallery,
+        'staffs': staff,
     }
     return render(request, 'website/index.html', context)
