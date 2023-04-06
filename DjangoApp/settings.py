1d72ef3265b3fb00a8b0b580b535798a79ecb9e7
sudaneee <99607513+sudaneee@users.noreply.github.com>
2023-04-06 07:42:51 +0100

    New ip added to allowed host

--- DjangoApp/settings.py
+++ DjangoApp/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-*&=8))+08y71!-%!qyk%lp!wwk-gt@wthl)%u%dibl&3j-c6uj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['178.79.178.160', 'localhost', '127.0.0.1', 'alabrar-fashion.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'alabrar-fashion.com', '212.71.251.137']
 
 
 # Application definition
