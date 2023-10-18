8ae9b530d3fa6801c99b146abd1d12de4f78c1ef
sudaneee <99607513+sudaneee@users.noreply.github.com>
2023-10-18 15:41:15 +0100

    falsy

--- DjangoApp/settings.py
+++ DjangoApp/settings.py
@@ -26,7 +26,7 @@ environ.Env.read_env()
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'www.alabrar-fashion.com', 'alabrar-fashion.com', '194.195.221.54']
 
