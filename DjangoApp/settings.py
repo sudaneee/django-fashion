992ec26e4613d2f0a57a3fe5d2ec2ad389be8216
sudaneee <99607513+sudaneee@users.noreply.github.com>
2023-10-20 09:56:34 +0100

    debug mode

--- DjangoApp/settings.py
+++ DjangoApp/settings.py
@@ -26,7 +26,7 @@ environ.Env.read_env()
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'www.alabrar-fashion.com', 'alabrar-fashion.com', '194.195.221.54', 'fashionhouse.applikuapp.com']
 
